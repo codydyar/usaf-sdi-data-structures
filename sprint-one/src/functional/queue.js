d0164e0f21b5d6085fc236a64048a83456cba7b7
Cody Dyar <codydyar@gmail.com>
2021-03-05 18:22:05 +0100

    added queue solution

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,38 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
 
+  var storage = {};
+  var endOfQueue = 0;
+  var frontOfQueue = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    endOfQueue++;
+    storage[endOfQueue] = value;
+    //key:value
+    // [{1: some value}]
+    // obj.key == value
+    // obj.1 = value
   };
 
   someInstance.dequeue = function() {
+    if(someInstance.size() === 0){
+      return;
+    } else {
+
+      frontOfQueue++;
+      var result = storage[frontOfQueue];
+      delete storage[frontOfQueue];
+      return result;
+      // (last item - size) + 1 = [0+ 1] = 1
+      // delete obj.1 => removes our first key property
+
+    }
   };
 
   someInstance.size = function() {
+    return endOfQueue - frontOfQueue;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    if(size === 0){
+      return;
+    } else {
+      var result = storage[size];
+      delete storage[size];
+      size = size -1;
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
