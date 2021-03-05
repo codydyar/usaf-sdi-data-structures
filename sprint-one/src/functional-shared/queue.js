38f5da87edb00bb22246a71f6d64606076c4bfc0
Cody Dyar <codydyar@gmail.com>
2021-03-05 22:17:02 +0100

    added queue functional-shared init incomp solut

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = {
+      queueHome: {},
+      endOfQueue: 0,
+      frontOfQueue: 0
+  };
+
+  extend(queue, queueMethods);
+  return queue;
+
 };
 
-var queueMethods = {};
+var extend = function(addToQueue, takeFromQueue){
+  for (var key in takeFromQueue){
+    addToQueue[key] = takeFromQueue[key]
+  }
+};
+
+var queueMethods = {
+
+  enqueue: function(value){
+    this.endOfQueue++;
+    this.queueHome[this.endOfQueue] = value;
+  },
 
 
+  dequeue: function() {
+    if (this.size() === 0) {
+     return;
+    } else {
+
+      frontOfQueue++;
+      var result = this.queueHome[this.frontOfQueue - 1];
+      delete this.queueHome[this.frontOfQueue - 1];
+      return result;
+
+    }
+  },
+
+  size: function() {
+   return endOfQueue - frontOfQueue;
+  }
+
+};
